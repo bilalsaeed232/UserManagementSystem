9ccb69a2dbafc42a38716800a263991890e8aeea
Bilal Saeed <bilalsaeed232@gmail.com>
2025-01-19 22:35:10 +0100

    adds: user login api endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const User = require("../models/User")
 
 const router = express.Router()
@@ -15,6 +16,31 @@ router.post("/register", async (req, res) => {
         res.status(201).json({message: "User created successfully!"})
     } catch (error) {
         res.status(500).json({ error: error.message })
+    }
+})
+
+// Login User
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body
+
+    try {
+        const user = await User.findOne({email})
+        // user not found
+        if (!user) {
+            return res.status(404).json({message:"User not found"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        // wrong password
+        if(!isMatch) {
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        // genereate jwt and send in payload
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, { expiresIn: "1h"})
+        res.status(200).json({ token })
+    } catch (error) {
+        res.status(500).json({error: error.message})    
     }
 })
 
