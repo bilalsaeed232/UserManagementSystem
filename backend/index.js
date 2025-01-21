d3106bf63c0c897336f62159d169558b71ff30d9
Bilal Saeed <bilalsaeed232@gmail.com>
2025-01-21 10:16:37 +0100

    adds: cors options

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,17 @@ const sessionRoutes = require("./routes/sessionRoutes")
 
 dotenv.config();
 
+// Allow requests from specific origins
+const corsOptions = {
+    origin: 'http://localhost:3000', // Frontend app's URL (or use '*' to allow all origins)
+    methods: 'GET,POST',  // Methods allowed
+    allowedHeaders: 'Content-Type,Authorization',  // Allowed headers
+    credentials: true  // Enable cookies/credentials if necessary
+};
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/api/users", userRoutes)
 app.use("/api/sessions", sessionRoutes)
 app.use("/uploads", express.static("uploads"))
